d3b4c867d8b30f52068ea13255605722b8e5744b
leticiaporto <leticiaportosoares@gmail.com>
2023-11-23 20:37:27 -0300

    feat: exemplos aula 14

---
+++ aula17/exemplo1/app.js
@@ -0,0 +1,50 @@
+//Variável recebendo texto
+let frase = 'Alô alunos da Turma 7 da Arnia'
+frase = 'Arnia Escola de Programação'
+console.log(frase)
+
+//Variavel recebendo número
+let numero = 1
+console.log(numero)
+
+let numeroDecimal = 1.5
+console.log(numeroDecimal)
+
+//somar 2 numeros 
+let soma = 2 + 2
+console.log(soma)
+
+//subtracao 2 numeros 
+let subtracao = 10 - 5
+console.log(subtracao)
+
+//multiplicacao
+let multiplicacao = 2 * 5
+console.log(multiplicacao)
+
+//divisao
+let divisao = 30 / 2
+console.log(divisao)
+
+//potenciação (5 elevado à 2)
+let potenciacao = 5 ** 2
+console.log(potenciacao)
+
+//resto de uma divisão
+let resto = 9 % 2
+console.log(resto)
+
+let soma3 = 3 + 8 + 9
+console.log(soma3)
+
+let somaTiposDiferentes = 3 + 'Arnia'
+console.log(somaTiposDiferentes)
+
+let somaNumeros = '1' + '1'
+console.log(somaNumeros)
+
+let soma4 = 2 + '2'
+console.log(soma4)
+
+let nomeCompleto = 'Letícia ' + 'Porto ' + 'Soares'
+console.log(nomeCompleto)
