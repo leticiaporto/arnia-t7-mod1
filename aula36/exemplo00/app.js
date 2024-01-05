4026b3325c44bba1619ef251a90efab80682e781
leticiaporto <leticiaportosoares@gmail.com>
2024-01-05 20:08:49 -0300

    feat: exemplos aula 36

--- aula36/exemplo00/app.js
+++ aula36/exemplo00/app.js
@@ -1,9 +1,8 @@
+
 const timestamp = (new Date()).getTime()
-const publicKey = 'SUA_PUBLIC_KEY_AQUI'
-const privateKey = 'SUA_PRIVATE_KEY_AQUI'
+const publicKey = 'SUA_CHAVE_PUBLICA_AQUI'
+const privateKey = 'SUA_CHAVE_PRIVADA_AQUI'
 const hash = timestamp + privateKey + publicKey
 const hashMd5 = MD5.generate(hash)
 
 const promise = fetch(`https://gateway.marvel.com:443/v1/public/characters?apikey=${publicKey}&ts=${timestamp}&hash=${hashMd5}`)
-
-console.log(promise)
