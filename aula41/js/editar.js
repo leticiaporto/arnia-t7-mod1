1b8278139205b9940f7eaa563eb40fb9f333095f
leticiaporto <leticiaportosoares@gmail.com>
2024-01-15 20:09:26 -0300

    feat: exemplos aula 41

--- aula41/js/editar.js
+++ aula41/js/editar.js
@@ -1,3 +1,41 @@
+//recuperando o formulário da página HTML através do DOM
+const formulario = document.querySelector('#formulario')
+let id = null
+
+formulario.addEventListener('submit', async (e) => {    
+    e.preventDefault() // evitar que as informações do fomulario sejam enviadas pra URL
+
+    //recuperar as informações do formulario
+    const titulo = formulario.elements['titulo'].value
+    const autor = formulario.elements['autor'].value
+    const ano = formulario.elements['ano'].value
+
+    //montar o objeto livro
+    const objetoLivro = {
+        titulo, //titulo: titulo
+        autor, 
+        anoLancamento: ano
+    }
+
+    //chamar a função que edita o livro na API
+    await editarLivro(id, objetoLivro)
+
+    //redirecionar para a tela de listagem 
+    window.location = '../index.html'
+})
+
+//editar o livro na API através do ID
+const editarLivro = async (id, livro) => {
+    await fetch(`http://localhost:3000/livros/${id}`, {
+        method: 'PUT',
+        headers: { //informações adicionais (formato dos dados: fixo)
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(livro)
+    })
+}
+
 //buscar o livro na API e retornar o livro
 const getLivro = async (id) => {
     const request = await fetch(`http://localhost:3000/livros/${id}`)
@@ -14,13 +52,13 @@ const carregarLivroFormulario = (livro) => {
 }
 
 const carregarDados = async () => {
-    //recupera parâmetros da URL (formato de string)
+    //1º recupera parâmetros da URL (formato de string)
     const parametros = window.location.search
     console.log(parametros)
     //transforma os parametros para o formato de objeto da classe URLSearchParams
     const objetoParametros = new URLSearchParams(parametros)
     //acessa o parâmetro desejado (id)
-    const id = objetoParametros.get('id')
+    id = objetoParametros.get('id')
     console.log(id)
 
     //chama a função para buscar o livro na API 
