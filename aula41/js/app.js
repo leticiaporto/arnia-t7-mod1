848b11b779ab1eb5f2f0edfe0beed348f12451c6
leticiaporto <leticiaportosoares@gmail.com>
2024-01-16 20:09:02 -0300

    feat: exemplos aula 42

--- aula41/js/app.js
+++ aula41/js/app.js
@@ -1,3 +1,13 @@
+//função chamada ao clicar no botão "Excluir" do item da listagem. Recebe o ID do livro clicado
+const excluirLivro = async (id) => {
+    await fetch(`http://localhost:3000/livros/${id}`, { 
+        method: 'DELETE'
+    })
+
+    //chama a função que atualiza a listagem
+    carregarDados()
+}
+
 //função que busca os livros na API
 const getLivros = async () => {
     const dados = await fetch('http://localhost:3000/livros')
@@ -9,6 +19,7 @@ const getLivros = async () => {
 //função que renderiza os livros na página HTML, usando os registros armazenados na variavel "listaLivros"
 const mostrarLivros = (listaLivros) => {
     const bodyTabela = document.querySelector('tbody')
+    bodyTabela.innerHTML = ''
 
     //passa por cada elemento da lista de livros e acrescenta no tbody uma linha (com colunas) correspondente ao livro
     //a última coluna possui um link que redireciona para a tela de Edição passando o id do livro na URL
@@ -19,7 +30,10 @@ const mostrarLivros = (listaLivros) => {
                 <td>${livro.titulo}</td>
                 <td>${livro.autor}</td>
                 <td>${livro.anoLancamento}</td>
-                <td><a href="html/editar.html?id=${livro.id}">Editar</a><td>
+                <td>
+                    <a href="html/editar.html?id=${livro.id}">Editar</a>
+                    <button onclick="excluirLivro('${livro.id}')">Excluir</button>
+                <td>
             </tr>
         `
     })
@@ -29,7 +43,6 @@ const mostrarLivros = (listaLivros) => {
 const carregarDados = async () => {
     //chamada à função que busca os dados na API e salva o retorno na variavel listaLivros
     const listaLivros = await getLivros()
-    console.log(listaLivros)
 
     //chamada à função que renderiza os livros na página HTML passando a lista de livros como parâmetro
     mostrarLivros(listaLivros)
